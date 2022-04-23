514b687786968655a07611108d7a9f08992af0c3
Brandon Dora <77594102+B2andon@users.noreply.github.com>
2022-04-23 17:03:53 -0400

    Update and rename DensityMap:Fire-per-County to DensityMapFirePerCounty.r

--- DensityMapFirePerCounty.r
+++ DensityMapFirePerCounty.r
@@ -1,5 +1,7 @@
 ### Brandon Dora's Density plot of the Fires per County in California. 
 
+
+### Importing Library 
 library(readr)
 library(ggmap)
 library(ggplot2)
@@ -7,7 +9,13 @@ library(tibble)
 library(tidyverse)
 library(zipcode)
 library(remotes)
+
+# setting working directory 
 setwd("C:/Users/Brandon Dora/Desktop/DataMining")
+
+
+
+#implementing Ben's cleaned dataset
  testFrame <- read.csv("California_Fire_Incidents.csv")
   
   testFrame <- testFrame[,-2]
@@ -40,6 +48,8 @@ setwd("C:/Users/Brandon Dora/Desktop/DataMining")
   
   testFrame <- replace_na(testFrame, list(AirTankers = 0, CrewsInvolved = 0, Dozers = 0, Engines = 0, Fatalities = 0, Helicopters = 0, Injuries = 0, PersonnelInvolved = 0, StructuresDamaged = 0, StructuresDestroyed = 0, StructuresThreatened = 0, WaterTenders = 0))
 
+
+## creating and cleaning data frames to map out the state of california
   
 states <- map_data("state")
 California <- subset(states, region == "california")
@@ -56,6 +66,10 @@ californiaCounties$Longitude <- californiaCounties$long
 californiaCounties$Latitude <- californiaCounties$lat
 californiaCounties$long = NULL
 californiaCounties$lat = NULL
+
+### creating a new dataframe based on our original to focus on the counties, and the number of times a fire has occured in each of them. 
+
+
 testFrame2 <- table(testFrame$Counties)
 testFrame2
 
@@ -64,16 +78,30 @@ testFrame2 <- as.data.frame(testFrame2)
 testFrame2$Counties <- c("Alameda", "Alpine", "Amador", "Butte", "Calaveras", "Colusa", "Contra Costa", "Del Norte", "El Dorado", "Fresno", "Glenn", "Humboldt", "Inyo", "Kern", "Kings", "Lake", "Lassen", "Los Angeles", "Madera", "Marin", "Mariposa", "Mendocino", "Merced", "Mexico", "Modoc", "Mono", "Monterey", "Napa", "Nevada", "Orange", "Placer", "Plumas", "Riverside", "Sacramento", "San Benito", "San Bernardino", "San Diego", "San Joaquin", "San Luis Obispo", "San Mateo", "Santa Barbara", "Santa Clara", "Santa Cruz", "Shasta", "Sierra", "Siskiyou", "Solano", "Sonoma", "Stanislaus", "State of Nevada", "State of Oregon", "Sutter", "Tehama", "Trinity", "Tulare", "Tuolumne", "Ventura", "Yolo", "Yuba")
 testFrame2$FireCount <- testFrame2$V1
 testFrame2$V1 = NULL
-t
 
+
+
+### combining our new test frame with our county dataframe. 
 californiaCounties <- left_join(californiaCounties, testFrame2, by="Counties")
 californiaCounties[is.na(californiaCounties)] = 0
 
 
 
-
+# creating our state map
 caliFire <- ggplot(data = California, mapping = aes(x = Longitude, y = Latitude, group = group))
 caliFire <- caliFire + geom_polygon(data = californiaCounties, color = "white") + geom_polygon(color = "black", fill = NA)
+
+
+### the state map sectioned by counties
+caliFire 
+
+
+
+
+
+### adding our data and creating our 2d density map on top of a heatmap
+
+
 caliFire <- caliFire + coord_fixed(1.3)
 caliFire <- caliFire + geom_polygon(data = californiaCounties, aes(fill = FireCount))
 caliFire<- caliFire + ggtitle("Fire Count in California by County")
